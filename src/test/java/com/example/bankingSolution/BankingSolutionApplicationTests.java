ec6806711c23e8e6d0c37a52a1fb97f121adc40f
jevolo <jevolo@ttu.ee>
2024-04-14 14:54:15 +0300

    Initial commit

---
+++ src/test/java/com/example/bankingSolution/BankingSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
